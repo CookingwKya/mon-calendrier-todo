4e3959472884ad44ae43d63518e67bcb1ebe06ca
Abdoulaye Diallo <adiallo06102@gmail.com>
2025-10-20 18:19:38 +0200

    Update Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mon-calendrier-todo/', // IMPORTANT: Remplacez par le nom de votre repo
+  base: '/mon-calendrier-todo/', // REMPLACEZ par votre nom de repo exact
+  build: {
+    outDir: 'dist'
+  },
   server: {
     host: true
   }
